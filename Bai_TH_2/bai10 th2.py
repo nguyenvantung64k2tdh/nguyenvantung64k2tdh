af8ed7fce23adf077932e71c94f26fe4d4b88863
nguyenvantung64k2tdh <nvtff2005@gmail.com>
2024-11-29 15:28:32 +0700

    Add files via upload

---
+++ Bai_TH_2/bai10 th2.py
@@ -0,0 +1,6 @@
+print("sinh vien:Nguyen Van Tung MSV:235752021610118")
+a="hi i am NguyenVanTung"
+b=a.split()
+print (b)
+c=" ".join(b)
+print(c)
---
+++ Bai_TH_2/bai11 th2.py
@@ -0,0 +1,10 @@
+print("sinh vien:Nguyen Van Tung MSV:235752021610118")
+
+l=["NguyenVanTung"]
+k=[19]
+m=[]
+m.append(l);
+m.append(k);
+print (m)
+d={"hoten":l,"tuoi":k,'combine_list':m}
+print (d)
